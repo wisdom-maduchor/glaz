ec30987d9a60291eed240625744cf5fb6635d5ac
wisdom <technocraterpdevelopers@gmail.com>
2024-01-29 10:10:25 +0100

    i made changes on the GLAZ project

---
+++ src/app/orders/list-orders/list-orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-orders.component.html',
+  styleUrl: './list-orders.component.css'
+})
+export class ListOrdersComponent {
+
+}
